d1e5c341ca3c86a0cc9d8d3bf1d400d22679e3ca
Junnu759 <gthirupathyreddy@gmail.com>
2023-01-29 11:16:51 +0530

    added card & Header

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import "./Card.css";
+
+export default () => {
+    return(
+        <div className="Card-con">
+            <div className="Card-img">
+                <img src ="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICAxOTkuM0sgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00323848-dzabtdsfbc-portrait.jpg"></img>
+            </div>
+            <div className="Card-label">
+                <p>pathan The Action thriller </p>
+            </div>
+
+
+</div>
+    );
+};
